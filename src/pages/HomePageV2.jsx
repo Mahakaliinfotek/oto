e03ad6ad8a343db57dd180f3b95a4451cea59051
rishabhIndiazona <rishabh.indiazona@gmail.com>
2026-07-20 13:23:12 +0530

    fixed header import

--- src/pages/HomePageV2.jsx
+++ src/pages/HomePageV2.jsx
@@ -10,7 +10,7 @@ import DifferenceSection from "../components/homepagev2/DifferenceSection";
 import CoreOfferingsSection from "../components/homepagev2/CoreOfferingsSection";
 import StevedoringSection from "../components/homepagev2/StevedoringSection";
 import AchievementsSection from "../components/homepagev2/AchievementsSection";
-import HeaderV2 from "../components/common/headerV2";
+import HeaderV2 from "../components/common/HeaderV2";
 import ClientsVisionSection from "../components/homepagev2/ClientsVisionSection";
 import BusinessesSection from "../components/homepagev2/BusinessesSection";
 
